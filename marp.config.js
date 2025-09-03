eed99700da1992f91a5a93df462677508a8e1bd3
Brian Staber <brian.staber@gmail.com>
2025-09-03 15:15:29 +0200

    :recycle: Fix theme

---
+++ marp.config.js
@@ -0,0 +1,10 @@
+// marp.config.js
+module.exports = {
+  themeSet: [
+    './css/rosepine.css',
+    './css/rosepine-dawn.css',
+    './css/rosepine-moon.css',
+  ],
+  allowLocalFiles: true,
+  output: 'build',
+};
